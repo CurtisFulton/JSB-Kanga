2247f99aaaf75a7073ee6868a4d5d5f8f60e19d3
Curtis Fulton <Curtis.R.Fulton@Gmail.com>
2018-02-12 10:50:44 +1000

    Create main page layout
    The page layout for the website has been made (Header + footer), no
    styling other than positioning and size has been done yet.

--- server.js
+++ server.js
@@ -12,8 +12,20 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-app.use('/', (req, res) => {
+app.get('/', (req, res) => {
 	res.render('home');
+});
+
+app.get('/about', (req, res) => {
+	res.render('about');
+});
+
+app.get('/gallery', (req, res) => {
+	res.render('gallery');
+});
+
+app.get('/contact', (req, res) => {
+	res.render('contact');
 });
 
 app.listen(port, () => console.log("Listening on port " + port));
